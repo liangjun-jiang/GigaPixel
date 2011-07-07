7919485a49fd1cb61bbeaca109be5c86e8650735
Liangjun Jiang <ljiang@Liangjun-Jiangs-MacBook-Pro.local>
2011-07-07 12:35:52 -0400

    Initial Commit

---
+++ Pathology-interface/Pathology_interfaceAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  Pathology_interfaceAppDelegate.h
+//  Pathology-interface
+//
+//  Created by Liangjun Jiang on 7/7/11.
+//  Copyright 2011 Harvard University Extension School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Pathology_interfaceAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ Pathology-interface/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pathology-interface
+//
+//  Created by Liangjun Jiang on 7/7/11.
+//  Copyright 2011 Harvard University Extension School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
